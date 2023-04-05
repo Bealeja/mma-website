a56612cff945205ed1573d65f43fac6cc3617abf
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-04-05 13:33:24 +1000

    Map Methods for Main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -12,6 +12,13 @@ const fight288Link = "https://www.ufc.com/event/ufc-288";
 const xmartialLink = "https://www.xmartial.com/";
 
 function Main(props, ref) {
+  // Define an array of fight objects with their respective links and image sources
+  const fights = [
+    { link: fight286Link, src: fight286 },
+    { link: fight287Link, src: fight287 },
+    { link: fight288Link, src: fight288 },
+  ];
+
   return (
     <div ref={ref} className="main">
       <div className="landingpage">
@@ -20,58 +27,59 @@ function Main(props, ref) {
 
         {/* <!-- Local Fights (images)--> */}
         <div className="section-main">
-          <a href={fight286Link} className="medialink">
-            <img className="mediaimage" src={fight286} />
-          </a>
-
-          <a href={fight287Link} className="medialink">
-            <img className="mediaimage" src={fight287} />
-          </a>
-
-          <a href={fight288Link} className="medialink">
-            <img className="mediaimage" src={fight288} />
-          </a>
+          {/* Use map method to dynamically render fight images and links */}
+          {fights.map((fight, index) => (
+            <a href={fight.link} className="medialink" key={index}>
+              <img className="mediaimage" src={fight.src} />
+            </a>
+          ))}
         </div>
 
         {/* <!--Advertising Banner--> */}
         <div className="section">
           <div className="banner">
             <a href={xmartialLink}>
-              <span style={{ "--i": 1 }}>2</span>
-              <span style={{ "--i": 2 }}>5</span>
-              <span style={{ "--i": 3 }}>%</span>
-              <span style={{ "--i": 4 }}>&nbsp;</span>
-              <span style={{ "--i": 5 }}>o</span>
-              <span style={{ "--i": 6 }}>f</span>
-              <span style={{ "--i": 7 }}>f</span>
-              <span style={{ "--i": 8 }}>&nbsp;</span>
-              <span style={{ "--i": 9 }}>x</span>
-              <span style={{ "--i": 10 }}>m</span>
-              <span style={{ "--i": 11 }}>a</span>
-              <span style={{ "--i": 12 }}>r</span>
-              <span style={{ "--i": 13 }}>t</span>
-              <span style={{ "--i": 14 }}>i</span>
-              <span style={{ "--i": 15 }}>a</span>
-              <span style={{ "--i": 16 }}>l</span>
-              <span style={{ "--i": 17 }}>&nbsp;</span>
-              <span style={{ "--i": 18 }}>u</span>
-              <span style={{ "--i": 19 }}>s</span>
-              <span style={{ "--i": 20 }}>e</span>
-              <span style={{ "--i": 21 }}>&nbsp;</span>
-              <span style={{ "--i": 22 }}>c</span>
-              <span style={{ "--i": 23 }}>o</span>
-              <span style={{ "--i": 24 }}>d</span>
-              <span style={{ "--i": 25 }}>e</span>
-              <span style={{ "--i": 26 }}>:</span>
-              <span style={{ "--i": 27 }}>&nbsp;</span>
-              <span style={{ "--i": 28 }}>T</span>
-              <span style={{ "--i": 28 }}>a</span>
-              <span style={{ "--i": 28 }}>k</span>
-              <span style={{ "--i": 28 }}>e</span>
-              <span style={{ "--i": 28 }}>d</span>
-              <span style={{ "--i": 28 }}>o</span>
-              <span style={{ "--i": 28 }}>w</span>
-              <span style={{ "--i": 28 }}>n</span>
+              {[
+                { value: "2", i: 1 },
+                { value: "5", i: 2 },
+                { value: "%", i: 3 },
+                { value: " ", i: 4 },
+                { value: "o", i: 5 },
+                { value: "f", i: 6 },
+                { value: "f", i: 7 },
+                { value: " ", i: 8 },
+                { value: "x", i: 9 },
+                { value: "m", i: 10 },
+                { value: "a", i: 11 },
+                { value: "r", i: 12 },
+                { value: "t", i: 13 },
+                { value: "i", i: 14 },
+                { value: "a", i: 15 },
+                { value: "l", i: 16 },
+                { value: " ", i: 17 },
+                { value: "u", i: 18 },
+                { value: "s", i: 19 },
+                { value: "e", i: 20 },
+                { value: " ", i: 21 },
+                { value: "c", i: 22 },
+                { value: "o", i: 23 },
+                { value: "d", i: 24 },
+                { value: "e", i: 25 },
+                { value: ":", i: 26 },
+                { value: " ", i: 27 },
+                { value: "T", i: 28 },
+                { value: "a", i: 29 },
+                { value: "k", i: 30 },
+                { value: "e", i: 31 },
+                { value: "d", i: 32 },
+                { value: "o", i: 33 },
+                { value: "w", i: 34 },
+                { value: "n", i: 35 },
+              ].map((item) => (
+                <span key={item.i} style={{ "--i": item.i }}>
+                  {item.value}
+                </span>
+              ))}
             </a>
           </div>
         </div>
